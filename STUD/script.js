91408336f25f9de6ce9df91a4c731543d8c92d44
Daniil <daniil.astakhov_it@outlook.com>
2023-03-07 17:01:59 +0300

    1 commit

--- STUD/script.js
+++ STUD/script.js
@@ -1,33 +1,6 @@
-// n! = n * (n - 1) * (n - 2) * ...*1
+// Работа с датой
 
-function factorial(data) {
-    if (typeof(data) !== 'number' || !Number.isInteger(data)) {
-        console.log('Ошибочка');
-    }
+'use strict'
 
-    if (data >= 1) {
-        return data * factorial (data -1);
-    } else {
-        return 1
-    }
-}
-console.log(factorial(5))
+const now = new Date();
 
-
-function factorial(n) {
-    if (typeof(n) !== 'number' || !Number.isInteger(n)) {
-        return "Ошибка, проверьте данные";
-    }
-
-    if (n >= 1) {
-        return n * factorial(n - 1);
-    } else {
-        return 1;
-    }
-
-    // Более короткий вариант, который вы можете встретить
-    // Но не учитывает отрицательные значения
-    return n ? n * factorial(n - 1) : 1;
-}
-
-factorial(5)
